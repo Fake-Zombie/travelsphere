bda148755dd8d91e5ce09523d88c815656bc9f08
Ankur Rajak <ankurkrrajak@gmail.com>
2026-07-01 17:51:50 +0530

    final push corrected .env

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -7,11 +7,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-console.log("Cloudinary config check:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  secret_set: !!process.env.CLOUDINARY_API_SECRET,
-});
 
 const createUploader = (folder, resourceType = 'image') => {
   const storage = new CloudinaryStorage({
